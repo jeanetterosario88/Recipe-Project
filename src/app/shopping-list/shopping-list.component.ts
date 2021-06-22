44342398d6c2a4f1e7a70e5c023a11bf71a2669f
jeanetterosario88 <jeanetterosario88@gmail.com>
2021-06-22 18:16:03 -0400

    ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingrediant.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-    ingredients = [];
-    
+    ingredients: Ingredient[] = []; // of type Ingrediant (from model you imported)
+
+    new Ingredient('Apples', 5);
+    new Ingredient('Tomatos', 10);
+
   constructor() { }
 
   ngOnInit(): void {
